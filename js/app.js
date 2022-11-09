ae0c757ac26e736cb98eb05c7955d5ff019609ea
dangerb3 <dangerb@yandex.ru>
2022-11-09 18:35:11 +0300

    scroll, parallax work

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 window.addEventListener("scroll", (e) => {
-  document.body.style.cssText = `--scrollTop: ${this.scrollY}px`;
+  document.body.style.cssText += `--scrollTop: ${this.scrollY}px`; /* нужен именно += чтобы добавлять стили к имеющимся, а не заменять их */
 });
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+ScrollSmoother.create({
+  wrapper: ".wrapper",
+  content: ".content",
+});
+// not working
